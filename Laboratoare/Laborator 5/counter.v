28428588fb6f0c1f662cd73aa6e4bb82ab4bab89
dragosb04 <63968720+dragosb04@users.noreply.github.com>
2024-12-09 16:02:34 +0200

    Update counter.v

--- Laboratoare/Laborator 5/counter.v
+++ Laboratoare/Laborator 5/counter.v
@@ -1,81 +1,54 @@
 module counter #(
-    parameter WIDTH = 8,
-    parameter INITIAL_VALUE = 8'hFF
+	parameter w = 8,
+	parameter init = {w{1'b0}}
 )(
-    input wire clk,
-    input wire rst_b,
-    input wire c_up,
-    input wire clr,
-    output reg [WIDTH-1:0] q
+	input clk, rst_b, c_up, clr,
+	output reg [w-1:0]q
 );
 
-    always @(posedge clk or negedge rst_b) begin
-        if (!rst_b) begin
-            q <= INITIAL_VALUE;     
-        end else if (clr) begin
-            q <= INITIAL_VALUE;     
-        end else if (c_up) begin
-            q <= q + 1;             
-        end
-    end
+	always @(posedge clk or negedge rst_b) begin
+		if (!rst_b)
+			q <= init;
+		else if (clr)
+			q <= init;
+		else if (c_up)
+			q <= q + 1;
+	end
 
 endmodule
 
 module counter_tb;
 
-    parameter WIDTH = 8;
-    parameter INITIAL_VALUE = 8'hFF;
+	localparam CLK_PERIOD = 100;
+	localparam CLK_CYCLES = 6;
+	localparam RST_PULSE = 5;
+	
+	reg clk, rst_b, c_up, clr;
+	wire[7:0] q; 
 
-    reg clk;
-    reg rst_b;
-    reg c_up;
-    reg clr;
-    wire [WIDTH-1:0] q;
+	counter uut (.clk(clk), .rst_b(rst_b), .c_up(c_up), .clr(clr), .q(q));
+	
+	initial begin
+		clk = 1'b0;
+		repeat (2*CLK_CYCLES) #(CLK_PERIOD/2) clk = ~clk;
+	end
 
-    counter #(
-        .WIDTH(WIDTH),
-        .INITIAL_VALUE(INITIAL_VALUE)
-    ) dut (
-        .clk(clk),
-        .rst_b(rst_b),
-        .c_up(c_up),
-        .clr(clr),
-        .q(q)
-    );
+	initial begin 
+		rst_b = 1'b0;
+		#(RST_PULSE) rst_b = 1'b1;
+	end
 
-    initial begin
-        clk = 0;
-        forever #50 clk = ~clk; 
-    end
+	initial begin
 
-    initial begin
-		$display("clk rst_b c_up clr | q");
-		$monitor("%b    %b    %b    %b  | %b", clk, rst_b, c_up, clr, q);
+		c_up = 1;
+		clr = 0;
 
-        rst_b = 1;
-        c_up = 0;
-        clr = 0;
-
-        #10 rst_b = 0; 
-        #5 rst_b = 1;  
-
-        #100;
-
-        clr = 1; 
-        #100 clr = 0; 
-
-        #100;
-
-        c_up = 1; 
-        #200;     
-        c_up = 0; 
-
-        #10 rst_b = 0; 
-        #5 rst_b = 1;  
-
-        #100;
-
-        $stop;
-    end
+		#(2 * CLK_PERIOD) clr = 1;
+		#(CLK_PERIOD) clr = 0;
+		#(CLK_PERIOD) c_up = 0;
+		#(CLK_PERIOD) c_up = 1;
+	
+		$finish;
+	end
 
 endmodule
