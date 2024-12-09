a4c7f05913c62b55af5df2e840fa088753097bc7
dragosb04 <63968720+dragosb04@users.noreply.github.com>
2024-12-09 03:15:31 +0200

    Update add2b.v

--- Laboratoare/Laborator 4/add2b.v
+++ Laboratoare/Laborator 4/add2b.v
@@ -1,67 +1,53 @@
-module fac (
-    input x,
-    input y,
-    input ci,
-    output z,
-    output co
+module add2b(
+	input [1:0] x,
+	input [1:0] y,
+	input ci,
+	output [1:0] o, co
 );
-  assign z  = x ^ y ^ ci;
-  assign co = (x & y) | (x & ci) | (y & ci);
+
+	wire new_ci;
+
+	fac uut1 (x[0], y[0], ci, o[0], new_ci);
+	fac uut2 (x[1], y[1], new_ci, o[1], co);
 
 endmodule
 
-module add2b (
-    input [1:0] x,
-    input [1:0] y,
-    input ci,
-    output [1:0] o,
-    output co
+module fac (
+	input x, y, ci,
+	output z, co
 );
-  wire c1;
-  fac fac0 (
-      .x (x[0]),
-      .y (y[0]),
-      .ci(ci),
-      .z (o[0]),
-      .co(c1)
-  );
-  fac fac1 (
-      .x (x[1]),
-      .y (y[1]),
-      .ci(c1),
-      .z (o[1]),
-      .co(co)
-  );
+
+	assign z = x ^ y ^ ci;
+	assign co = (x & y) | (ci & (x ^ y));
 
 endmodule
 
 module add2b_tb;
 
-  reg [1:0] x;
-  reg [1:0] y;
-  reg ci;
-  wire [1:0] o;
-  wire co;
+	reg [1:0] x;
+	reg [1:0] y;
+	reg ci;
+	wire [1:0] o;
+	wire co;
 
-  add2b uut (
-      .x (x),
-      .y (y),
-      .ci(ci),
-      .o (o),
-      .co(co)
-  );
+	integer k;
+		
+	add2b uut (
+		.x(x),
+		.y(y),
+		.ci(ci),
+		.o(o),
+		.co(co)
+	);
 
-  integer i;
+	initial begin
+	
+	$display("Time\t x  \t y  \tci\t o \tco");
+	$monitor("%0t\t%b\t%b\t%b\t%b\t%b", $time, x, y, ci, o, co);
+		{x, y, ci} = 0;
+		for (k = 1; k < 32; k = k + 1)
+			#10 {x, y, ci} = k[4:0];
 
-  initial begin
-    $display("x  y  ci   | s  co");
-    $display("-----------|------");
-    for (i = 0; i < 32; i = i + 1) begin
-      {x, y, ci} = i;
-      #20;
-      $display("%b %b %b | %b %b", x, y, ci, o, co);
-    end
-
-  end
+	end
 
 endmodule
