87ddf60d7848f64d522823158948bf940ec945c4
dragosb04 <63968720+dragosb04@users.noreply.github.com>
2024-12-10 02:34:43 +0200

    Update sadd.v

--- Laboratoare/Laborator 6/sadd.v
+++ Laboratoare/Laborator 6/sadd.v
@@ -1,78 +1,97 @@
 module sadd (
-    input clk,
-    input rst_b,
-    input a,
-    input b,
-    output reg m
+	input clk, rst_b, x, y,
+	output reg o
 );
+	
+	reg carry;
+	reg state;
 
-  localparam S0_ST = 1'b0;
-  localparam S1_ST = 1'b1;
+	localparam S0_ST = 1'b0;
+	localparam S1_ST = 1'b1;
 
-  reg [1:0] st;
-  reg [1:0] st_next;
-
-  always @(*) begin
-    m = 1'b0;
-    case (st)
-      S0_ST:
-      if (a && b) begin
-        st_next = S1_ST;
-        m = 1'b0;
-      end else if (!a && !b) begin
-        st_next = S0_ST;
-        m = 1'b0;
-      end else begin
-        st_next = S0_ST;
-        m = 1'b1;
-      end
-      S1_ST:
-      if (a && b) begin
-        st_next = S1_ST;
-        m = 1'b1;
-      end else if (!a && !b) begin
-        st_next = S0_ST;
-        m = 1'b1;
-      end else begin
-        st_next = S1_ST;
-        m = 1'b0;
-      end
-    endcase
-  end
-  always @(posedge clk, negedge rst_b)
-    if (!rst_b) st <= S0_ST;
-    else st <= st_next;
+	always @ (posedge clk or negedge rst_b) begin
+		if (!rst_b) begin
+			state <= S0_ST;
+			carry <= 1'b0;
+		end
+		else begin
+			
+			case (state)
+				S0_ST: begin
+					if (x == 0 && y == 0) begin
+						state <= S0_ST;
+						carry <= 1'b0;
+					end
+					else if (x == 0 && y == 1) begin
+						state <= S0_ST;
+						carry <= 1'b0;
+					end
+					else if (x == 1 && y == 0) begin
+						state <= S0_ST;
+						carry <= 1'b0;
+					end
+					else begin
+						state <= S1_ST;
+						carry <= 1'b1;
+					end
+				end
+				S1_ST: begin
+					if (x == 0 && y == 0) begin
+						state <= S0_ST;
+						carry <= 1'b0;
+					end else if ((x == 0 && y == 1) || (x == 1 && y == 0)) begin
+						state <= S1_ST;
+						carry <= 1'b1;
+					end else if (x == 1 && y == 1) begin
+						state <= S1_ST;
+						carry <= 1'b1;
+					end					
+				end
+			endcase 
+		end
+	end
+	
+	always @(*) begin
+		o = x ^ y ^ carry;
+	end
 endmodule
 
 module sadd_tb;
-  reg clk;
-  reg rst_b;
-  reg [3:0] X;
-  reg [3:0] Y;
-  reg a;
-  reg b;
-  wire m;
 
-  sadd uut (
-      .clk(clk),
-      .rst_b(rst_b),
-      .a(a),
-      .b(b),
-      .m(m)
-  );
+	reg clk, rst_b, x, y;
+	wire o;
 
-  initial begin
-    clk = 0;
-    rst_b = 0;
-    X = 4'b0110;
-    Y = 4'b0011;
+	localparam CLK_CYCLES = 5;
+	localparam CLK_PERIOD = 50;
+	localparam RST_PULSE = 25;
 
-    {a, b} = 'b01;
-    #100{a, b} = 'b11;
-    #100{a, b} = 'b10;
-    #100{a, b} = 'b00;
+	sadd uut (
+		.clk(clk),
+		.rst_b(rst_b),
+		.x(x),
+		.y(y),
+		.o(o)
+	);
 
-    #20 $finish;
-  end
-  always #20 clk = ~clk;
+	initial begin
+		clk = 1'b0;
+		repeat (CLK_CYCLES*2) #(CLK_PERIOD) clk = ~clk; 
+	end
+
+	initial begin
+		rst_b = 1'b0;
+		#RST_PULSE rst_b = 1'b1;
+	end
+
+	initial begin
+		x = 1'b0;
+		y = 1'b1;
+
+		#(CLK_PERIOD*2) x = 1'b1;
+		#(CLK_PERIOD*2) y = 1'b0;
+		
+		#(CLK_PERIOD*2) x = 1'b0;
+
+	end
+
 endmodule
